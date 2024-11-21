252ba43f30a89d4240ebec653be2417fc64da079
RomeoOnGithub <168174189+RomeoOnGithub@users.noreply.github.com>
2024-11-21 12:54:07 +0100

    clicked digits appear individually on the display

--- scripts.js
+++ scripts.js
@@ -12,3 +12,25 @@ let operate = function(operatorIndex, value1, value2) {
     let operator = operators[operatorIndex];
     return operator(value1, value2);
 };
+
+//digit responsiveness 
+let zero = document.getElementById("0").value;
+
+let one = document.getElementById("1");
+let two = document.getElementById("2");
+let three = document.getElementById("3");
+let four = document.getElementById("4");
+let five = document.getElementById("5");
+let six = document.getElementById("6");
+let seven = document.getElementById("7");
+let eight = document.getElementById("8");
+let nine = document.getElementById("9");
+
+let digits = [zero, one, two, three, four, five, six, seven, eight, nine];
+
+const display = document.getElementById("display");
+
+const digitsButtons = document.getElementById("digits").addEventListener("click", function() {
+    display.innerHTML = event.target.value;
+});
+
